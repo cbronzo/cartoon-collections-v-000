be421a59a372e9dbaf2655226382e1f2bfa0e622
Catherine Bronzo <cbronzo@mac.com>
2018-06-19 02:27:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(dwarf_array)
   dwarf_array = ["Doc", "Dopey", "Bashful", "Grumpy"]
-  dwarf_array.each_with_index |name, index|
+  dwarf_array.each_with_index do |name, index|
   puts "#{index.to_i+1} #{name}"
-end
+  end
 end

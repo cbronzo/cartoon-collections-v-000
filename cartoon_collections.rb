e0955bfe9c9228a78d5ea824d3a00073df2a4947
Catherine Bronzo <cbronzo@mac.com>
2018-06-19 02:50:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |type|
-    type.include
+  array.include do |type|
+    puts type
   end
 end

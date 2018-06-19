da7d8afebd4a910aae22a4abf81f382136f7c830
Catherine Bronzo <cbronzo@mac.com>
2018-06-19 02:26:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
-
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def roll_call_dwarves(dwarf_array)
+  dwarf_array = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  dwarf_array.each_with_index |name, index|
+  puts "#{index.to_i+1} #{name}"
 end
